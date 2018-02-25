a9a0ff310f815cff6b2968aeebe839281975b538
ccortes <ccortes@microsoft.com>
2018-02-24 22:06:35 -0800

    Last Digit Fibonacci number V1.0

---
+++ DSA_Certificate/AlgoToolbox/Week2/LD_fibonacci_number_2/LD_fibonacci_number_2/LD_fibonacci_number.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD_fibonacci_number_2
+{
+    class LD_fibonacci_number
+    {
+        static Int64 calc_fibo(Int64 n)
+        {
+            if (n <= 1)
+                return n;
+            Int64 prev2 = 0;
+            Int64 prev1 = 1;
+            Int64 current = 2;
+            for (Int64 i = 2; i <= n; i++)
+            {
+                current = (prev2 + prev1) % 10;
+                prev2 = prev1;
+                prev1 = current;
+            }
+            return current;
+        }
+        static void Main(string[] args)
+        {
+            var ns = Console.ReadLine();
+            Int64 n = Int64.Parse(ns);
+            Console.WriteLine(calc_fibo(n));
+            //Console.WriteLine("Press any key to continue");
+            //Console.ReadLine();
+        }
+    }
+}
+
