a2fa56125436d3ca08e4519c7fab19203e4b9ef3
ccortes007 <ccortes@microsoft.com>
2018-03-10 09:31:12 -0800

    Adding Week 1

---
+++ DSA_Certificate/AlgoToolbox/Week1/MaxPairwiseProduct/MaxPairwiseProduct/MaxPairwiseProduct.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxPairwiseProduct
+{
+    class MaxPairwiseProduct
+    {
+        static Int64 pwp (Int64[] numbers)
+        {
+            Int64 result = 0;
+            Int64 max1 = Int64.MinValue;
+            Int64 max2 = Int64.MinValue;
+            for (int i=0; i<numbers.Length; i++)
+            {
+                if (numbers[i] > max1)
+                {
+                    max2 = max1;
+                    max1 = numbers[i];
+                }
+                //else if (max1 > numbers[i] & numbers[i] > max2) max2 = numbers[i];
+                else if (numbers[i] > max2) max2 = numbers[i];
+            }
+            result = max1 * max2;
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            var ns = Console.ReadLine();
+            Int64 n = Int64.Parse(ns);
+            Int64[] num = new Int64[n];
+            var input = Console.ReadLine();
+            var tokens = input.Split(' '); 
+            for (Int64 i=0; i<n; i++)
+            {
+                num[i] = Int64.Parse(tokens[i]);
+            }
+            Int64 res = pwp(num);
+            Console.WriteLine(res);
+            //Console.WriteLine("Press any key to continue");
+            //Console.ReadLine();
+            return;
+        }
+    }
+}
---
+++ DSA_Certificate/AlgoToolbox/Week1/MaxPairwiseProductTest/MaxPairwiseProductTest/MaxPairwiseProductTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxPairwiseProductTest
+{
+    class MaxPairwiseProductTest
+    {
+        static Int64 mpwpSlow(Int64[] numbers)
+        {
+            Int64 result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] * numbers[j] > result)
+                    {
+                        result = numbers[i] * numbers[j];
+                    }
+                }
+
+            }
+            return result;
+        }
+        static Int64 mpwp(Int64[] numbers)
+        {
+            Int64 result = 0;
+            Int64 max1 = Int64.MinValue;
+            Int64 max2 = Int64.MinValue;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max1)
+                {
+                    max2 = max1;
+                    max1 = numbers[i];
+                }
+                else if (numbers[i] > max2) max2 = numbers[i];
+            }
+            result = max1 * max2;
+            return result;
+        }
+        static Random _r = new Random();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int n = (_r.Next() % 20) + 2;
+                Int64[] num = new Int64[n];
+                for (int i=0; i<num.Length; i++)
+                {
+                    num[i] = _r.Next() % 100000;
+                }
+                Int64 res_slow = mpwpSlow(num);
+                Int64 res_fast = mpwp(num);
+                Console.WriteLine(n);
+                for (int i = 0; i < num.Length; i++)
+                {
+                    Console.Write(num[i]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+                if (res_slow == res_fast)
+                    Console.WriteLine("OK");
+                else
+                {
+                    Console.WriteLine("Error");
+                    break;
+                }
+            }
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
